648b4e3f6459fce5c0a0c68f8a8500fc3d1b6399
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-03-29 00:18:58 +0800

    Cell function added

--- main.js
+++ main.js
@@ -1,36 +1,28 @@
+//Gameboard has 3x3 dimension cell
+// creatte 2d array board object
+//createt function that draw out the players symbol
+//create function that printsBoard;
+
+// Cell is an object that has value , read value and add value function needed
+
+
 function Gameboard() {
  const rows = 3;
- const columns = 3;
+ const columns =  3;
  const board = [];
-
-
- for(let i = 0;  i < rows; i++) {
-  board[i] = [];
-  for(let j = 0; j < columns; j++) {
-   board[i].push(Cell());
-  }
- };
-
- co= dropToken = (player) => {
-  const availableCell = board.filter((row) => row[column].getValue() === 0).map(row => row[column]);
- }
-
- const printBoard = () => {
-
- }
 }
 
 function Cell() {
- let value = ""
+ let value = 0;
 
+ 
  const addToken = (player) => {
-  value = player
+  value = player;
  }
-
  const getValue = () => value;
 
  return {
-  addToken,
+  addSymbol,
   getValue
  }
 }

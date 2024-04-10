e60bf5b49c32ee046f8f4830ebca53a4e67372aa
sliver <125364267+slivermarkr@users.noreply.github.com>
2024-04-10 22:37:59 +0800

    printbaord and drawToken

--- script.js
+++ script.js
@@ -9,8 +9,38 @@ function Gameboard() {
    board[i].push(Cell());
   }
  }
+
+ const getBoard = () => board;
+
+
+ const drawToken = (row,column,playerToken) => {
+  board.filter(row => {
+   if(row[column].getValue()=== "") return;
+  })
+  board[row][column].addToken(playerToken);
+ }
+
+ const printBoard = () => {
+  const boardWithCellValues = board.map(row => {
+   row.map(cell => cell.getToken())
+  });
+  console.log(boardWithCellValues)
+ }
+ return {
+  getBoard,
+  drawToken,
+  printBoard
+ }
 }
 
 function Cell() {
- 
+ let value  = "";
+
+ const addToken = (playerToken) => {
+  value = playerToken
+ }
+
+ const getToken = () => value;
+
+ return {addToken, getToken};
 }
